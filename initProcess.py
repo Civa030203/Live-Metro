9badd7892bbac982143861a327ce1f1be497702c
Civa030203 <seungjeajoo@gmail.com>
2023-12-23 20:55:21 +0900

    Selenium Ssibal

--- initProcess.py
+++ initProcess.py
@@ -2,6 +2,7 @@ from pyvirtualdisplay import Display
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 class initProcess():
     def start_options(url):
